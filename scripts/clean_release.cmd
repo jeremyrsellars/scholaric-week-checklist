e58e863283952a864bd0500afbcd9b95b4c0ed90
Jeremy R. Sellars <jeremy.sellars@softekinc.com>
2018-07-21 19:51:36 -0500

    Add publish scripts
    For Github pages.

---
+++ scripts/clean_release.cmd
@@ -0,0 +1,1 @@
+del "%~dp0..\src\school-week-checklist\resources\public\js\compiled\school_week_checklist.js"
---
+++ scripts/pub_doc.cmd
@@ -0,0 +1,10 @@
+:: @ECHO OFF
+SETLOCAL
+set root=%~dp0..
+pushd "%~dp0"
+set doc=%root%\docs
+if exist "%doc%" RD "%doc%" /q/s
+if not exist "%doc%" MD "%doc%"
+xcopy /s /y  /exclude:exclusions.txt "%root%\src\school-week-checklist\resources\public\*" "%doc%\"
+FOR %%f in (%doc%\*.html) DO powershell -f %~dp0versionize-html.ps1 %%f
+popd
